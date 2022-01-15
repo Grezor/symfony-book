b315e422fee69fb0842cfdd874b6958cea530222
Geoffrey <grezor.gamer@gmail.com>
2022-01-15 23:10:41 +0100

    add controller

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConferenceController extends AbstractController
+{
+    #[Route('/hello/{name}', name: 'homepage')]
+    public function index(string $name = ''): Response
+    {
+        $greet = '';
+        if ($name) {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
+        return new Response(
+            <<<EOF
+                <html>
+                <body>
+                $greet
+                    <img src="/images/under-construction.gif" />
+                </body>
+                </html>
+            EOF
+        );
+    }
+}
