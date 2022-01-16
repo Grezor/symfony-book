1b7acafbb780e350aa8a6a6f6677a6c317195056
Geoffrey <grezor.gamer@gmail.com>
2022-01-16 18:00:43 +0100

    add twig
    - installer twig
    - twig dans un controller
    - pagination commentaire
    - optimisation controller

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,28 +2,47 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Twig\Environment;
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/hello/{name}', name: 'homepage')]
-    public function index(string $name = ''): Response
+    private $twig; 
+
+    public function __construct(Environment $twig)
     {
-        $greet = '';
-        if ($name) {
-            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
-        }
-        return new Response(
-            <<<EOF
-                <html>
-                <body>
-                $greet
-                    <img src="/images/under-construction.gif" />
-                </body>
-                </html>
-            EOF
-        );
+        $this->twig = $twig;
+    }
+
+    #[Route('/', name: 'homepage')]
+    public function index(ConferenceRepository $conferenceRepository): Response
+    {
+        return new Response($this->twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll()
+        ]));
+    }
+
+    #[Route('/conference/{id}', name: 'conference')]
+    public function show(
+        Request $request,
+        Conference $conference, 
+        CommentRepository $commentRepository
+    ): Response 
+    {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+        
+        return new Response($this->twig->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+        ]));
     }
 }
